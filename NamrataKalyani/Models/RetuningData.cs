7b91a812c0040b49baa0aeb670ee4f4467350436
yawar2019 <yawarali17@gmail.com>
2021-10-02 12:30:23 +0530

    committed

--- NamrataKalyani/Models/RetuningData.cs
+++ NamrataKalyani/Models/RetuningData.cs
@@ -3,28 +3,29 @@ using Dapper;
 using System.Data.SqlClient;
 using System.Data;
 using System.Collections.Generic;
+using System.Configuration;
 
 namespace NamrataKalyani.Models
 {
     public static class RetuningData
     {
-     
+
+
         public static T AddOrSave<T>(String spName, DynamicParameters param)
         {
-            SqlConnection con = new SqlConnection(@"Data Source=AZAM-PC\SQLEXPRESS; Integrated Security=true; Initial Catalog=SUNDIGNOSTIC");
-            var result = (T)Convert.ChangeType(con.Execute(spName, param, commandType: CommandType.StoredProcedure),typeof(T));
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlCon"].ToString());
+            var result = (T)Convert.ChangeType(con.Execute(spName, param, commandType: CommandType.StoredProcedure), typeof(T));
             return result;
         }
         public static T ReturnSingleValue<T>(String spName, DynamicParameters param)
         {
-            SqlConnection con = new SqlConnection(@"Data Source=AZAM-PC\SQLEXPRESS; Integrated Security=true; Initial Catalog=SUNDIGNOSTIC");
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlCon"].ToString());
             var result = (T)Convert.ChangeType(con.ExecuteScalar(spName, param, commandType: CommandType.StoredProcedure), typeof(T));
             return result;
         }
         public static IEnumerable<T> ReturnigList<T>(String spName, DynamicParameters param)
         {
-            SqlConnection con = new SqlConnection(@"Data Source=AZAM-PC\SQLEXPRESS; Integrated Security=true; Initial Catalog=SUNDIGNOSTIC");
-            // var result = (T)Convert.ChangeType(con.Execute(spName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlCon"].ToString());
             return con.Query<T>(spName, param, commandType: CommandType.StoredProcedure);
         }
 
